04d35e74602b542b755e9a36e79bc45c39d49f88
AZhao <alanzzhao@gmail.com>
2018-02-05 00:11:46 -0500

    added a submissions folder

--- data_munge.py
+++ data_munge.py
@@ -92,6 +92,21 @@ def pre_process_data(df, enforce_cols=None):
 
     return df
 
+
+def make_country_sub(preds, test_feat, country):
+    # make sure we code the country correctly
+    country_codes = ['A', 'B', 'C']
+
+    # get just the poor probabilities
+    country_sub = pd.DataFrame(data=preds[:, 1],  # proba p=1
+                               columns=['poor'],
+                               index=test_feat.index)
+
+    # add the country code for joining later
+    country_sub["country"] = country
+    return country_sub[["country", "poor"]]
+
+
 aX_h_train = pre_process_data(a_h_train.drop('poor', axis=1))
 ay_h_train = np.ravel(a_h_train.poor)
 
